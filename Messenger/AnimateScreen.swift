4fea85cd9a77c282eb453aa37fbb85f525d4e8b5
imerkulenko <merkmerk23@gmail.com>
2022-10-20 00:28:42 +0500

    Added Gif screen

---
+++ Messenger/AnimateScreen.swift
@@ -0,0 +1,20 @@
+//
+//  AnimateScreen.swift
+//  Messenger
+//
+//  Created by Илья Меркуленко on 20.10.2022.
+//
+
+import SwiftUI
+
+struct AnimateScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimateScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimateScreen()
+    }
+}
