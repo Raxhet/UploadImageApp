be585d8053ab2801401b3736eedcd9df3c032857
imerkulenko <merkmerk23@gmail.com>
2022-10-21 23:21:54 +0500

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Илья Меркуленко on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
